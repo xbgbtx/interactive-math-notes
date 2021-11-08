d46250e16d814bcddd25eb1fc93384abde18357f
xbgbtx <78358155+xbgbtx@users.noreply.github.com>
2021-11-08 18:16:05 +0000

    Add publis.sh

---
+++ publish.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+DIR=$(readlink -m $1)
+
+cd $DIR/..
+
+if [[ $(git status -s) ]]
+then
+   echo "The working directory is dirty. Please commit any pending changes."
+   exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public origin/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Copying project submodules"
+rsync -av --exclude='*.git*' project_submodules/ public
+
+echo "Generating site"
+hugo -D
+
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+git push origin gh-pages
