d8086d36797770e7192dbe36c198f3c1a1f283e8
xbgbtx <78358155+xbgbtx@users.noreply.github.com>
2021-11-26 18:59:12 +0000

    Properly scale iframe with size change

--- static/js/p5js-sketch-iframe-scale.js
+++ static/js/p5js-sketch-iframe-scale.js
@@ -1,16 +1,15 @@
 const unscaled_sketch_size = 512;
 
 window.addEventListener('resize', 
-    e => resize_all_sketches (calc_sketch_width()),
+    e => resize_all_sketches (),
     true);
 document.addEventListener('DOMContentLoaded',
-    e => resize_all_sketches(calc_sketch_width()),
+    e => resize_all_sketches(),
     false);
 
-function calc_sketch_width ()
+function calc_sketch_width (sketch_div)
 {
-    const main = document.getElementsByTagName("main")[0];
-    const w = main.offsetWidth;
+    const w = sketch_div.offsetWidth;
 
     return Math.min ( w, unscaled_sketch_size );
 }
@@ -20,7 +19,7 @@ function resize_all_sketches (size)
     const sketch_divs = document.getElementsByClassName("p5js-sketch");
 
     for ( const d of sketch_divs )
-        resize_sketch_div ( d, size );
+        resize_sketch_div ( d, calc_sketch_width(d) );
 }
 
 function resize_sketch_div (sketch_div, new_width)
@@ -28,7 +27,7 @@ function resize_sketch_div (sketch_div, new_width)
     const scale = new_width / unscaled_sketch_size;
     const sketch_iframe = sketch_div.getElementsByTagName ( "iframe" )[0];
 
-    console.log(`${new_width}, ${scale}`);
+    sketch_iframe.style.transformOrigin = "0 0";
     sketch_iframe.style.scale = scale;
 }
 
